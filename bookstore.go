333cac7493891a172af2155ffd4266b9f05c7f66
Piero Mamberti <piero.mamberti@gmail.com>
2021-04-20 08:35:44 +0100

    review up to chapter 3

--- bookstore.go
+++ bookstore.go
@@ -5,7 +5,7 @@ import "fmt"
 // Book represents information about a Book
 type Book struct {
 	Title           string
-	Author          string
+	Author          []string
 	Series          int
 	SeriesTitle     string
 	PriceCents      int
@@ -14,24 +14,42 @@ type Book struct {
 	NetPrice        int
 }
 
+var books []Book
+
 func netPrice(b Book) int {
 	discount := float64(b.PriceCents) * float64(b.DiscountPercent) / 100
 	return b.PriceCents - int(discount)
 }
 
+func AddBook(c []Book, b Book) []Book {
+	c = append(c, b)
+	return c
+}
+
 func main() {
+	books = []Book{}
+
 	b := Book{
 		Title:           "An Elegant Puzzle",
-		Author:          "Will Larson",
+		Author:          []string{"Will Larson"},
 		PriceCents:      2999,
 		DiscountPercent: 10,
 	}
 
-	b.NetPrice = netPrice(b)
+	b1 := Book{
+		Title:           "Staff Engineer",
+		Author:          []string{"Will Larson"},
+		PriceCents:      2499,
+		DiscountPercent: 10,
+	}
 
-	fmt.Printf(
-		`Author: %v
-Title: %v
-Price: %v
-Discount: %v%%`, b.Author, b.Title, b.PriceCents/100, b.DiscountPercent)
+	b.NetPrice = netPrice(b)
+	b1.NetPrice = netPrice(b1)
+
+	books = AddBook(books, b)
+	books = AddBook(books, b1)
+
+	for _, b := range books {
+		fmt.Printf("Author(s): %v \nTitle: %v \n ============= \n", b.Author, b.Title)
+	}
 }
